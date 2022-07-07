c91eec5178cc68b2ade15a7b53f5eb5be37f256c
notgoodcom <kkun4647@gmail.com>
2022-07-08 03:50:18 +0900

    이미지연결

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
 $(function(){
+
     $("#content").load("content.html"); //content불러오기
+
+
+    // top배너 불러오기
+
+    window.onload = function () {
+        startLoadFile();
+    };
+    
+    function startLoadFile(){
+        $.ajax({
+            url: './img.json',
+            type: 'GET',
+            dataType : 'json',
+            success : function (data) {
+                createImages(data)
+            }
+        });
+    }
+    
+    // JSON 포멧 데이터 처리
+    function createImages(objImageInfo) {
+        var images = objImageInfo.topBanner;
+        var strDOM = "";
+        for (var i = 0; i < images.length; i++) {
+            // N번째 이미지 정보를 구하기
+            var image = images[i];
+    
+            //  N번째 이미지 패널을 생성
+            strDOM += '<div class="imgWrap">';
+            strDOM += '    '<img src="' + image.url + '">';
+            strDOM += '    '<p class="title">' + image.name + ''</p>';
+            strDOM += ''</div>';
+        }
+        // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
+        var $imageContainer = $(".topBanner");
+            $imageContainer.append(strDOM);
+    }
 });
