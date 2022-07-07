361e3f39874dec3cc9109b8473f504ec0d385f28
notgoodcom <kkun4647@gmail.com>
2022-07-08 04:48:00 +0900

    nav 추가

--- js/main.js
+++ js/main.js
@@ -38,4 +38,50 @@ $(function(){
         var $imageContainer = $(".topBanner");
             $imageContainer.append(strDOM);
     }
+
+    // slider nav 불러오기
+
+    window.onload = function () {
+        startNav();
+    };
+    
+    function startNav(){
+        $.ajax({
+            url: './img.json',
+            type: 'GET',
+            dataType : 'json',
+            success : function (data) {
+                createSlider(data)
+            }
+        });
+    }
+    
+    // JSON 포멧 데이터 처리
+    function createSlider(objImage) {
+        var slide = objImage.bannerNav;
+        var strDOM = "";
+        for (var i = 0; i < slide.length; i++) {
+            // N번째 이미지 정보를 구하기
+            var sd = slide[i];
+    
+            //  N번째 이미지 패널을 생성
+            strDOM += '<div class="slider" style="background-image:url(' + sd.imageUrl + ')">';
+            strDOM += '<div class="wrap">';
+            strDOM += '<div class="sliderInfo">';
+            strDOM += '<div class="cate">' + sd.cate + '</div>';
+            strDOM += '<div class="line"></div>';
+            strDOM += '<div class="slideTitle">' + sd.title + '</div>';
+            strDOM += '<div class="slideTxt">' + sd.txt + '</div>';
+            strDOM += 'a class="btn btn-line" href="' + sd.url + '"></div>';
+            strDOM += '<img src="' + image.url + '" alt="' + image.name +'">';
+            strDOM += '</div>';
+            strDOM += '</div>';
+            strDOM += '</div>';
+        }
+            
+        // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
+        var $imageContainer = $(".sliderWrap");
+            $imageContainer.append(strDOM);
+    }
+
 });
