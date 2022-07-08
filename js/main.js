92985f8fdafa6d1508e5b202d07b9d0630524709
notgoodcom <kkun4647@gmail.com>
2022-07-08 23:48:43 +0900

    헤더수정

--- js/main.js
+++ js/main.js
@@ -63,17 +63,26 @@ $(function(){
         }
 
 
+    // gnb style
+
+    $(".wrapper").on("mouseover",".menu li a",function(){
+        var _this = $(this);
+        $(".menu li a").removeClass("on");
+        _this.addClass("on");
+    });
+
+
     // subMenu open & close
 
 
     $(".wrapper").on("mouseover", ".menu li a, header.top .gnb .subGnb", function(){
         var sub = $("header.top .gnb .subGnb");
-        sub.show(); 
+        sub.show(200); 
     });
 
     $(".wrapper").on("mouseout", "header.top .gnb .subGnb", function(){
         var sub = $("header.top .gnb .subGnb");
-        sub.hide(); 
+        sub.hide(200); 
     });
 
 });
