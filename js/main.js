1c41d56b15590b5a9438e163032a3e9ba6fa1430
notgoodcom <kkun4647@gmail.com>
2022-07-08 22:15:20 +0900

    js수정

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ $(function(){
                 dataType : 'json',
                 success : function (data) {
                     createImages(data)
+                    createSlider(data)
                 }
             });
         }
@@ -66,31 +67,31 @@ $(function(){
     //         });
     //     }
         
-    //     // JSON 포멧 데이터 처리
-    //     function createSlider(objImage) {
-    //         var slide = objImage.bannerNav;
-    //         var strDOM = "";
-    //         for (var i = 0; i < slide.length; i++) {
-    //             // N번째 이미지 정보를 구하기
-    //             var sd = slide[i];
+        // JSON 포멧 데이터 처리
+        function createSlider(objImage) {
+            var slide = objImage.bannerNav;
+            var strDOM = "";
+            for (var i = 0; i < slide.length; i++) {
+                // N번째 이미지 정보를 구하기
+                var sd = slide[i];
         
-    //             //  N번째 이미지 패널을 생성
-    //             strDOM += '<div class="slider" style="background-image:url(' + sd.imageUrl + ')">';
-    //             strDOM += '<div class="wrap">';
-    //             strDOM += '<div class="sliderInfo">';
-    //             strDOM += '<div class="cate">' + sd.cate + '</div>';
-    //             strDOM += '<div class="line"></div>';
-    //             strDOM += '<div class="slideTitle">' + sd.title + '</div>';
-    //             strDOM += '<div class="slideTxt">' + sd.txt + '</div>';
-    //             strDOM += '<a class="btn btn-line" href="' + sd.url + '">VIEW MORE</a>';
-    //             strDOM += '</div>';
-    //             strDOM += '</div>';
-    //             strDOM += '</div>';
-    //         }
+                //  N번째 이미지 패널을 생성
+                strDOM += '<div class="slider" style="background-image:url(' + sd.imageUrl + ')">';
+                strDOM += '<div class="wrap">';
+                strDOM += '<div class="sliderInfo">';
+                strDOM += '<div class="cate">' + sd.cate + '</div>';
+                strDOM += '<div class="line"></div>';
+                strDOM += '<div class="slideTitle">' + sd.title + '</div>';
+                strDOM += '<div class="slideTxt">' + sd.txt + '</div>';
+                strDOM += '<a class="btn btn-line" href="' + sd.url + '">VIEW MORE</a>';
+                strDOM += '</div>';
+                strDOM += '</div>';
+                strDOM += '</div>';
+            }
                 
-    //         // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
-    //         var $imageContainer = $(".sliderWrap");
-    //             $imageContainer.append(strDOM);
-    //     }
+            // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
+            var $imageContainer = $(".sliderWrap");
+                $imageContainer.append(strDOM);
+        }
 
 });
