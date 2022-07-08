35164fa2753a364c17414770a81ace8605587c48
notgoodcom <kkun4647@gmail.com>
2022-07-08 13:07:59 +0900

    서브메뉴 수정

--- js/main.js
+++ js/main.js
@@ -9,35 +9,45 @@ $(function(){
         startLoadFile();
     };
     
-    function startLoadFile(){
-        $.ajax({
-            url: './img.json',
-            type: 'GET',
-            dataType : 'json',
-            success : function (data) {
-                createImages(data)
-            }
-        });
-    }
-    
-    // JSON 포멧 데이터 처리
-    function createImages(objImageInfo) {
-        var images = objImageInfo.topBanner;
-        var strDOM = "";
-        for (var i = 0; i < images.length; i++) {
-            // N번째 이미지 정보를 구하기
-            var image = images[i];
-    
-            //  N번째 이미지 패널을 생성
-            strDOM += '<a class="imgWrap" href="' + image.src + '"style="background-image:url(' + image.url + ')">';
-            // strDOM += '<img src="' + image.url + '" alt="' + image.name +'">';
-            strDOM += '</a>';
+        function startLoadFile(){
+            $.ajax({
+                url: './img.json',
+                type: 'GET',
+                dataType : 'json',
+                success : function (data) {
+                    createImages(data)
+                }
+            });
         }
-            
-        // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
-        var $imageContainer = $(".topBanner");
-            $imageContainer.append(strDOM);
-    }
+        
+        // JSON 포멧 데이터 처리
+        function createImages(objImageInfo) {
+            var images = objImageInfo.topBanner;
+            var strDOM = "";
+            for (var i = 0; i < images.length; i++) {
+                // N번째 이미지 정보를 구하기
+                var image = images[i];
+        
+                //  N번째 이미지 패널을 생성
+                strDOM += '<a class="imgWrap" href="' + image.src + '"style="background-image:url(' + image.url + ')">';
+                // strDOM += '<img src="' + image.url + '" alt="' + image.name +'">';
+                strDOM += '</a>';
+            }
+                
+            // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
+            var $imageContainer = $(".topBanner");
+                $imageContainer.append(strDOM);
+        }
+
+
+    // subMenu open
+
+    $(".wrapper").on("mousehover", ".menu li a", function(){
+        var _this = $(this);
+        var sub = $(".subGnb");
+        sub.show(); 
+    })
+
 
     // slider nav 불러오기
 
@@ -45,42 +55,42 @@ $(function(){
         startNav();
     };
     
-    function startNav(){
-        $.ajax({
-            url: './img.json',
-            type: 'GET',
-            dataType : 'json',
-            success : function (data) {
-                createSlider(data)
-            }
-        });
-    }
-    
-    // JSON 포멧 데이터 처리
-    function createSlider(objImage) {
-        var slide = objImage.bannerNav;
-        var strDOM = "";
-        for (var i = 0; i < slide.length; i++) {
-            // N번째 이미지 정보를 구하기
-            var sd = slide[i];
-    
-            //  N번째 이미지 패널을 생성
-            strDOM += '<div class="slider" style="background-image:url(' + sd.imageUrl + ')">';
-            strDOM += '<div class="wrap">';
-            strDOM += '<div class="sliderInfo">';
-            strDOM += '<div class="cate">' + sd.cate + '</div>';
-            strDOM += '<div class="line"></div>';
-            strDOM += '<div class="slideTitle">' + sd.title + '</div>';
-            strDOM += '<div class="slideTxt">' + sd.txt + '</div>';
-            strDOM += '<a class="btn btn-line" href="' + sd.url + '">VIEW MORE</a>';
-            strDOM += '</div>';
-            strDOM += '</div>';
-            strDOM += '</div>';
+        function startNav(){
+            $.ajax({
+                url: './img.json',
+                type: 'GET',
+                dataType : 'json',
+                success : function (data) {
+                    createSlider(data)
+                }
+            });
         }
-            
-        // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
-        var $imageContainer = $(".sliderWrap");
-            $imageContainer.append(strDOM);
-    }
+        
+        // JSON 포멧 데이터 처리
+        function createSlider(objImage) {
+            var slide = objImage.bannerNav;
+            var strDOM = "";
+            for (var i = 0; i < slide.length; i++) {
+                // N번째 이미지 정보를 구하기
+                var sd = slide[i];
+        
+                //  N번째 이미지 패널을 생성
+                strDOM += '<div class="slider" style="background-image:url(' + sd.imageUrl + ')">';
+                strDOM += '<div class="wrap">';
+                strDOM += '<div class="sliderInfo">';
+                strDOM += '<div class="cate">' + sd.cate + '</div>';
+                strDOM += '<div class="line"></div>';
+                strDOM += '<div class="slideTitle">' + sd.title + '</div>';
+                strDOM += '<div class="slideTxt">' + sd.txt + '</div>';
+                strDOM += '<a class="btn btn-line" href="' + sd.url + '">VIEW MORE</a>';
+                strDOM += '</div>';
+                strDOM += '</div>';
+                strDOM += '</div>';
+            }
+                
+            // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
+            var $imageContainer = $(".sliderWrap");
+                $imageContainer.append(strDOM);
+        }
 
 });
