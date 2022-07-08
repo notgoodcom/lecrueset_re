7e317366e27eed63773da94ca429963fe8e0f22f
notgoodcom <kkun4647@gmail.com>
2022-07-08 22:20:46 +0900

    js수정

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function(){
     $("#content").load("content.html"); //content불러오기
 
 
-    // top배너 불러오기
+    // 이미지json 불러오기
 
     window.onload = function () {
         startLoadFile();
@@ -55,9 +55,11 @@ $(function(){
                 
             // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
             var $bnContainer = $(".topBanner");
-            $bnContainer.append(strDOM);
             var $imageContainer = $(".sliderWrap");
+
+            $bnContainer.append(strDOM);
             $imageContainer.append(strDOM);
+
         }
 
 
@@ -69,49 +71,13 @@ $(function(){
         sub.show(); 
     })
 
+    // subMenu close
 
-    // slider nav 불러오기
+    $(".wrapper").on("mouseover", ".menu li a, .subGnb", function(){
+        var _this = $(this);
+        var sub = $("header.top .gnb .subGnb");
+        sub.hide(); 
+    })
 
-    // window.onload = function () {
-    //     startNav();
-    // };
-    
-    //     function startNav(){
-    //         $.ajax({
-    //             url: './img.json',
-    //             type: 'GET',
-    //             dataType : 'json',
-    //             success : function (data) {
-    //                 createSlider(data)
-    //             }
-    //         });
-    //     }
-        
-        // JSON 포멧 데이터 처리
-        // function createSlider(objImage) {
-        //     var slide = objImage.bannerNav;
-        //     var strDOM = "";
-        //     for (var i = 0; i < slide.length; i++) {
-        //         // N번째 이미지 정보를 구하기
-        //         var sd = slide[i];
-        
-        //         //  N번째 이미지 패널을 생성
-        //         strDOM += '<div class="slider" style="background-image:url(' + sd.imageUrl + ')">';
-        //         strDOM += '<div class="wrap">';
-        //         strDOM += '<div class="sliderInfo">';
-        //         strDOM += '<div class="cate">' + sd.cate + '</div>';
-        //         strDOM += '<div class="line"></div>';
-        //         strDOM += '<div class="slideTitle">' + sd.title + '</div>';
-        //         strDOM += '<div class="slideTxt">' + sd.txt + '</div>';
-        //         strDOM += '<a class="btn btn-line" href="' + sd.url + '">VIEW MORE</a>';
-        //         strDOM += '</div>';
-        //         strDOM += '</div>';
-        //         strDOM += '</div>';
-        //     }
-                
-        //     // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
-        //     var $imageContainer = $(".sliderWrap");
-        //         $imageContainer.append(strDOM);
-        // }
 
 });
