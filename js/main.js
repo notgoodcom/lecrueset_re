c3f4aeeaea56ed19b9d3037eebb1f3ba96ec0da4
notgoodcom <kkun4647@gmail.com>
2022-07-09 17:35:08 +0900

    js테스트

--- js/main.js
+++ js/main.js
@@ -29,52 +29,60 @@ $(function(){
             var strDOM02 = "";
             var strDOM03 = "";
 
-            for (var i = 0; i < images.length; i++) {
-                // N번째 이미지 정보를 구하기
-                var image = images[i];
+            // for (var i = 0; i < images.length; i++) {
+            //     // N번째 이미지 정보를 구하기
+            //     var image = images[i];
         
-                //  N번째 이미지 패널을 생성
-                strDOM += '<a class="imgWrap" href="' + image.src + '"style="background-image:url(' + image.url + ')">';
-                // strDOM += '<img src="' + image.url + '" alt="' + image.name +'">';
-                strDOM += '</a>';
-            }
+            //     //  N번째 이미지 패널을 생성
+            //     strDOM += '<a class="imgWrap" href="' + image.src + '"style="background-image:url(' + image.url + ')">';
+            //     // strDOM += '<img src="' + image.url + '" alt="' + image.name +'">';
+            //     strDOM += '</a>';
+            // }
 
-            for (var i = 0; i < slide.length; i++) {
-                // N번째 이미지 정보를 구하기
-                var sd = slide[i];
+            // for (var i = 0; i < slide.length; i++) {
+            //     // N번째 이미지 정보를 구하기
+            //     var sd = slide[i];
         
-                //  N번째 이미지 패널을 생성
-                strDOM02 += '<div class="slider" style="background-image:url(' + sd.imageUrl + ')">';
-                strDOM02 += '<div class="wrap">';
-                strDOM02 += '<div class="sliderInfo">';
-                strDOM02 += '<div class="cate">' + sd.cate + '</div>';
-                strDOM02 += '<div class="line"></div>';
-                strDOM02 += '<div class="slideTitle">' + sd.title + '</div>';
-                strDOM02 += '<div class="slideTxt">' + sd.txt + '</div>';
-                strDOM02 += '<a class="btn btn-line" href="' + sd.url + '">VIEW MORE</a>';
-                strDOM02 += '</div>';
-                strDOM02 += '</div>';
-                strDOM02 += '</div>';
-            }
+            //     //  N번째 이미지 패널을 생성
+            //     strDOM02 += '<div class="slider" style="background-image:url(' + sd.imageUrl + ')">';
+            //     strDOM02 += '<div class="wrap">';
+            //     strDOM02 += '<div class="sliderInfo">';
+            //     strDOM02 += '<div class="cate">' + sd.cate + '</div>';
+            //     strDOM02 += '<div class="line"></div>';
+            //     strDOM02 += '<div class="slideTitle">' + sd.title + '</div>';
+            //     strDOM02 += '<div class="slideTxt">' + sd.txt + '</div>';
+            //     strDOM02 += '<a class="btn btn-line" href="' + sd.url + '">VIEW MORE</a>';
+            //     strDOM02 += '</div>';
+            //     strDOM02 += '</div>';
+            //     strDOM02 += '</div>';
+            // }
 
-            for (var i = 0; i < bn.length; i++) {
-                // N번째 이미지 정보를 구하기
-                var banner = bn[i];
+            // for (var i = 0; i < bn.length; i++) {
+            //     // N번째 이미지 정보를 구하기
+            //     var banner = bn[i];
         
-                //  N번째 이미지 패널을 생성
-                strDOM03 += '<a class="bnBox" href="' + banner.src + '">';
-                strDOM03 += '<img src="' + banner.imageUrl + '" alt="' + banner.name +'">';
-                strDOM03 += '</a>';
-            }
+            //     //  N번째 이미지 패널을 생성
+            //     strDOM03 += '<a class="bnBox" href="' + banner.src + '">';
+            //     strDOM03 += '<img src="' + banner.imageUrl + '" alt="' + banner.name +'">';
+            //     strDOM03 += '</a>';
+            // }
                 
+            var strDOM = image.name;
+            var strDOM02 = sd.name;
+            var strDOM03 = banner.name;
+
             // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
             var $bnContainer = $(".topBanner");
             var $imageContainer = $(".sliderWrap");
             var $midBnContainer = $(".bannerWrap");
 
-            $bnContainer.append(strDOM);
-            $imageContainer.append(strDOM02);
-            $midBnContainer.append(strDOM03);
+            // $bnContainer.append(strDOM);
+            // $imageContainer.append(strDOM02);
+            // $midBnContainer.append(strDOM03);
+
+            console.log(strDOM);
+            console.log(strDOM02);
+            console.log(strDOM03);
 
         }
 
