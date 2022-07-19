90e74396c882987c4c6df88d369410ba5a5b7214
notgoodcom <kkun4647@gmail.com>
2022-07-20 03:10:27 +0900

    수정

--- js/main.js
+++ js/main.js
@@ -66,4 +66,16 @@ $(function(){
         }
     });
 
+    //event cateTab
+
+    $(".wrapper").on("click","..eventTab li button",function(){
+        var _this = $(this);
+        $(".eventTab li button").parents("li").removeClass("on")
+        if(_this.parents("li").hasClass("on")){
+            $(".eventTab li button").parents("li").removeClass("on")
+        } else {
+            _this.parents("li").addClass("on");
+        }
+    });
+
 });
