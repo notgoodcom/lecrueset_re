d35f87d370ef335fd56414ad532c80cfb29e2f90
notgoodcom <kkun4647@gmail.com>
2022-07-20 02:22:02 +0900

    js수정

--- js/main.js
+++ js/main.js
@@ -7,30 +7,9 @@ $(function(){
 
     // price comma
 
-    function commaSeparateNumber(val){
-        val = val.toString().replace(/,/g, ''); //remove existing commas first
-        var valRZ = val.replace(/^0+/, ''); //remove leading zeros, optional
-        var valSplit = valRZ.split('.'); //then separate decimals
-            
-        while (/(\d+)(\d{3})/.test(valSplit[0].toString())){
-            valSplit[0] = valSplit[0].toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
-        }
-        
-        if(valSplit.length == 2){ //if there were decimals
-            val = valSplit[0] + "." + valSplit[1]; //add decimals back
-        }else{
-            val = valSplit[0]; }
-        
-        return val;
-    }
-
-    $(".discountBefore").each(function(){
-        $(this).html(commaSeparateNumber($(this).html()));
-    });
-    
-
-    Number(10000).toLocaleString('en');
-
+	$('.discountBefore').each(function(index){
+		$(this).text($(this).text().split(/(?=(?:\d{3})+(?:\.|$))/g).join(','));
+	});
 
     // gnb style
 
